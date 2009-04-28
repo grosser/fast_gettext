f7da3cc5b345542f20305d1a94e4bcc2ded7ab9b
grosser <grosser.michael@gmail.com>
2009-04-28 21:35:50 +0200

    fixed migration

--- examples/db/migration.rb
+++ examples/db/migration.rb
@@ -2,17 +2,17 @@ class CreateTranslationTables < ActiveRecord::Migration
   def self.up
     create_table :translation_keys do |t|
       t.string :key, :unique=>true, :null=>false
-      t.add_index :key
       t.timestamps
     end
-    
+    add_index :translation_keys, :key #I am not sure if this helps....
+
     create_table :translation_texts do |t|
       t.text :text
       t.string :locale
       t.integer :translation_key_id, :null=>false
-      t.add_index :translation_key_id
       t.timestamps
     end
+    add_index :translation_texts, :translation_key_id
   end
 
   def self.down
