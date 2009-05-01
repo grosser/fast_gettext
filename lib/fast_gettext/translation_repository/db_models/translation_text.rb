ebe74a92c58a66992cb2ec7fec1a13386c7ee2ed
grosser <grosser.michael@gmail.com>
2009-05-01 19:06:51 +0200

    TranslationText can have an empty text

--- lib/fast_gettext/translation_repository/db_models/translation_text.rb
+++ lib/fast_gettext/translation_repository/db_models/translation_text.rb
@@ -2,7 +2,7 @@ module FastGettext::TranslationRepository
   module DbModels
     class TranslationText < ActiveRecord::Base
       belongs_to :key, :class_name=>'TranslationKey'
-      validates_presence_of :locale, :text
+      validates_presence_of :locale
       validates_uniqueness_of :locale, :scope=>:translation_key_id
     end
   end
