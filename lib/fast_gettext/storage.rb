1fe59f53dc3ac41c9479026d78f6c4ed32a0d2ba
grosser <michael@grosser.it>
2014-07-08 19:19:25 -0700

    move cache class

--- lib/fast_gettext/storage.rb
+++ lib/fast_gettext/storage.rb
@@ -1,54 +1,10 @@
+require 'fast_gettext/cache'
+
 module FastGettext
   # Responsibility:
   #  - store data threadsave
   #  - provide error messages when repositories are unconfigured
   #  - accept/reject locales that are set by the user
-  class Cache
-    def initialize
-      @store = {}
-      reload!
-    end
-
-    def fetch(key)
-      translation = @current[key]
-      if translation.nil? # uncached
-        @current[key] = yield || false # TODO get rid of this false hack and cache :missing
-      else
-        translation
-      end
-    end
-
-    # TODO only used for tests, maybe if-else around it ...
-    def []=(key, value)
-      @current[key] = value
-    end
-
-    # TODO only used for tests, maybe if-else around it ...
-    def [](key)
-      @current[key]
-    end
-
-    # key performance gain:
-    # - no need to lookup locale on each translation
-    # - no need to lookup text_domain on each translation
-    # - super-simple hash lookup
-    def switch_to(text_domain, locale)
-      @store[text_domain] ||= {}
-      @store[text_domain][locale] ||= {}
-      @store[text_domain][locale][""] = false # ignore gettext meta key when translating
-      @current = @store[text_domain][locale]
-    end
-
-    def delete(key)
-      @current.delete(key)
-    end
-
-    def reload!
-      @current = {}
-      @current[""] = false
-    end
-  end
-
   module Storage
     class NoTextDomainConfigured < RuntimeError
       def to_s
