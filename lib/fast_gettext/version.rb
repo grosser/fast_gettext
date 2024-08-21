e3a7baee6c3af3ff350709233daf843bb497d2aa
Michael Grosser <michael@grosser.it>
2024-08-20 22:41:47 -0700

    v3.1.0

--- lib/fast_gettext/version.rb
+++ lib/fast_gettext/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module FastGettext
-  VERSION = Version = '3.0.0' # rubocop:disable Naming/ConstantName
+  VERSION = Version = '3.1.0' # rubocop:disable Naming/ConstantName
 end
